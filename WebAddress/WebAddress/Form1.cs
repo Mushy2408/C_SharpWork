3614a825fddc52fafa1e0c90e41201531579aad3
Mushy2408 <127207722+Mushy2408@users.noreply.github.com>
2024-11-15 14:17:53 -0500

    changes

--- WebAddress/WebAddress/Form1.cs
+++ WebAddress/WebAddress/Form1.cs
@@ -16,11 +16,36 @@ namespace WebAddress
 
         private void Btn1_Click(object sender, EventArgs e)
         {
-            
+
             //Symbols = "!","@","#","$","%","^","&","*","(",")","_","+","-","=","`","~","{","[","}","]","","|",";",":","'","",",","<",".","/", "?"
-            Name = txtInput.Text;
-           uhhhhhhh
-            txtOutput.Text = "www." + Name + ".com";
+            //string Name = txtInput.Text;
+
+            //txtOutput.Text = "www." + Name + ".com";
+            // Get the business name from the input textbox
+            string businessName = txtInput.Text;
+
+            // Initialize the web address with "www."
+            string webAddress = "www.";
+
+            // Loop through each character in the business name
+            foreach (char ch in businessName)
+            {
+                // Only add the character if it is not a space
+                if (ch != ' ')
+                {
+                    webAddress += ch;
+                }
+            }
+
+            // Add ".com" to the end of the web address
+            webAddress += ".com";
+
+            // Set the output textbox with the generated web address
+            txtOutput.Text = webAddress;
+
+
+
+
         }
     }
 }
--- WebAddress/WebAddress/obj/Debug/net8.0-windows/WebAddress.AssemblyInfo.cs
+++ WebAddress/WebAddress/obj/Debug/net8.0-windows/WebAddress.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAddress")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3f8390ca42ba1cf8bbfc810ee35db12a1a782cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9086c2660cfd2a197570cfcbabbc0b3642148916")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAddress")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAddress")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
